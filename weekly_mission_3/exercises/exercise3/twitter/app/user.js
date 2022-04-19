b3a7265716e33cf37a03d0ac97828882542c04f3
felix <josevelazco134@gmail.com>
2022-04-19 14:39:41 -0600

    Crear clase User

---
+++ weekly_mission_3/exercises/exercise3/twitter/app/user.js
@@ -0,0 +1,10 @@
+class User{
+  constructor(id, name, username, bio, dateCreated, lastUpdated){
+    this.id = id;
+    this.name = name;
+    this.username = username;
+    this.bio = bio;
+    this.dateCreated = dateCreated;
+    this.lastUpdated = lastUpdated;
+  }
+}
