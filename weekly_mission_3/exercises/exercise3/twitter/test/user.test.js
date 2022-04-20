9f3d71a3eb5e5b4866cbad18997a505c0556b185
felix <josevelazco134@gmail.com>
2022-04-19 20:02:51 -0600

    Agregar prueba para crear un objeto

--- weekly_mission_3/exercises/exercise3/twitter/test/user.test.js
+++ weekly_mission_3/exercises/exercise3/twitter/test/user.test.js
@@ -1,7 +1,11 @@
+const User = require("./../app/user.js")
+
 describe("Unit suite para la clase usuario", ()=> {
   test("1) Crear un objeto", ()=>{
-    const result = 2+1;
-    expect(result).toBe(10);
+    const felixUser = new User(1, "felix velazco", "felixvv7", "just a normal guy");
+    expect(felixUser.name).toBe("felix velazco");
+    expect(felixUser.username).toBe("felixvv7");
+    expect(felixUser.bio).toBe("just a normal guy");
   })
 })
 
