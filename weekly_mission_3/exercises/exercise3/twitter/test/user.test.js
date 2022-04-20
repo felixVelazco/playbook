d37fa6d810e2102fdaf3bce5427078d307120fbe
felix <josevelazco134@gmail.com>
2022-04-19 20:14:59 -0600

    Agregar pruebas a los metodos get

--- weekly_mission_3/exercises/exercise3/twitter/test/user.test.js
+++ weekly_mission_3/exercises/exercise3/twitter/test/user.test.js
@@ -1,11 +1,29 @@
 const User = require("./../app/user.js")
 
+
 describe("Unit suite para la clase usuario", ()=> {
+  const felixUser = new User(1, "felix velazco", "felixvv7", "just a normal guy");
+  
   test("1) Crear un objeto", ()=>{
-    const felixUser = new User(1, "felix velazco", "felixvv7", "just a normal guy");
     expect(felixUser.name).toBe("felix velazco");
     expect(felixUser.username).toBe("felixvv7");
     expect(felixUser.bio).toBe("just a normal guy");
+  })
+
+  test("2) Test getUsername funtion", ()=>{
+    expect(felixUser.getUsername()).toBe("felix velazco");  
+  })
+  
+  test("3) Test getBio function", ()=>{
+    expect(felixUser.getBio()).toBe("just a normal guy");  
+  })
+
+  test("4) Test getDateCreated function", ()=>{
+    expect(felixUser.getDateCreated()).not.toBeUndefined();  
+  })
+ 
+  test("5) Test getLastUpdated function", ()=>{
+    expect(felixUser.getLastUpdated()).not.toBeUndefined();  
   })
 })
 
