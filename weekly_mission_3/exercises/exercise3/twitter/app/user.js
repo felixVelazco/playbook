0a30f15f9eb92d53cf29ade6a20681cb1f70f512
felix <josevelazco134@gmail.com>
2022-04-19 20:02:02 -0600

    Agregar fechas en formato 'Date'

--- weekly_mission_3/exercises/exercise3/twitter/app/user.js
+++ weekly_mission_3/exercises/exercise3/twitter/app/user.js
@@ -1,11 +1,11 @@
 class User{
-  constructor(id, name, username, bio, dateCreated, lastUpdated){
+  constructor(id, name, username, bio){
     this.id = id;
     this.name = name;
     this.username = username;
     this.bio = bio;
-    this.dateCreated = dateCreated;
-    this.lastUpdated = lastUpdated;
+    this.dateCreated = new Date();
+    this.lastUpdated = new Date();
   }
 }
 
