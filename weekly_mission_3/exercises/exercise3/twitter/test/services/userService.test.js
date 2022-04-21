48a8d51a4eff9f7aac709ee8849a21dd5b0bedc1
felix <josevelazco134@gmail.com>
2022-04-21 00:50:46 -0600

    Agregar prueba para funcion 'getInfo'

--- weekly_mission_3/exercises/exercise3/twitter/test/services/userService.test.js
+++ weekly_mission_3/exercises/exercise3/twitter/test/services/userService.test.js
@@ -2,9 +2,17 @@ const UserService = require("./../../app/services/userService");
 
 describe('Unit test suite',() =>{
   test('1) Crear un objeto', () => {
-    const felix = UserService.create(1, "felixvv7", "felix velazco");
-    expect(felix.id).toBe(1);
-    expect(felix.username).toBe("felixvv7");
-    expect(felix.name).toBe("felix velazco");
+    const userFelix = UserService.create(1, "felixvv7", "felix velazco");
+    expect(userFelix.id).toBe(1);
+    expect(userFelix.username).toBe("felixvv7");
+    expect(userFelix.name).toBe("felix velazco");
+  })
+  test('2) Try get Functions', () => {
+    const userFelix = UserService.create(1, "felixvv7", "felix velazco");
+    const userInfo = UserService.getInfo(userFelix);
+    expect(userInfo[0]).toBe(1);
+    expect(userInfo[1]).toBe("felixvv7");
+    expect(userInfo[2]).toBe("felix velazco");
+    expect(userInfo[3]).toBe("Default");
   })
 });
