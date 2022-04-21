59e0d39138c7eb8ce4fee4b0ab32fb1020f04978
felix <josevelazco134@gmail.com>
2022-04-21 16:20:40 -0600

    Agregar prueba 2, donde uno de los parametros es null

--- weekly_mission_3/exercises/exercise3/twitter/test/views/userView.test.js
+++ weekly_mission_3/exercises/exercise3/twitter/test/views/userView.test.js
@@ -6,5 +6,13 @@ describe('Tests for UserView',() =>{
     //Al usar toMatch, verifica que la frase exista en una cadena mas larga.
     expect(user.error).toMatch('payload no existe');
   })
-
+  test('2) Username, name or ID is null', () => {
+    const payload = {
+      id: null,
+      username: "felixvv7",
+      name: "felix velazco"
+    };
+    const user = UserView.createUser(payload);
+    expect(user.error).toBe("necesitan tener un valor válido");
+  })
 })
