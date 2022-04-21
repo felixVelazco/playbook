4c05078a97d2fe6f8bf567000325cfd3dd45ea03
felix <josevelazco134@gmail.com>
2022-04-21 00:51:09 -0600

    Agregar funcion getInfo

--- weekly_mission_3/exercises/exercise3/twitter/app/services/userService.js
+++ weekly_mission_3/exercises/exercise3/twitter/app/services/userService.js
@@ -4,6 +4,10 @@ class UserService{
   static create(id, username, name){
     return new User(id, name, username, "Default");
   }
+  static getInfo(user){
+    const list = [user.id, user.username, user.name, user.bio]
+    return list;
+  }
 }
 
 module.exports = UserService;
