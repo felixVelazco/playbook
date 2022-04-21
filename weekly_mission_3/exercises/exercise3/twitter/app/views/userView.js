bc6655d07e2f7870ac9e4d6dc76fe294eb26d676
felix <josevelazco134@gmail.com>
2022-04-21 16:51:32 -0600

    Agregar caso donde parametros de payload sean nulos

--- weekly_mission_3/exercises/exercise3/twitter/app/views/userView.js
+++ weekly_mission_3/exercises/exercise3/twitter/app/views/userView.js
@@ -2,7 +2,20 @@ class UserView{
   static createUser(payload){
     if(payload === null){
       return {error: "Error: El payload no existe. Inténtelo nuevamente"}
-    }    
+    }
+    //Solo aplica con 3 parametros
+    // if(payload.id === null || payload.username === null || payload.name ===null){
+    //   return {error: "Error: Los valores del payload necesitan tener un valor válido. Inténtelo nuevamente"}
+    // }
+
+    //Con n parametros
+     const list = Object.values(payload);
+     
+     for(let i=0; i<list.length; i++){
+      if(list[i]===null)
+        return {error: "Error: Los valores del payload necesitan tener un valor válido. Inténtelo nuevamente"}
+     }
+    return {error: "no error"};
   }
 }
 
