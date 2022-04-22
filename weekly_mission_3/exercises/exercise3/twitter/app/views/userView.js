6c2c9974d97a6f4f2a5b7a36fb2b800d7593f2f0
felix <josevelazco134@gmail.com>
2022-04-21 21:37:39 -0600

    Modificar estructura del codigo solamente

--- weekly_mission_3/exercises/exercise3/twitter/app/views/userView.js
+++ weekly_mission_3/exercises/exercise3/twitter/app/views/userView.js
@@ -1,12 +1,13 @@
+const UserService = require("./../services/userService")
+
 class UserView{
   static createUser(payload){
-    if(payload === null){
+    if(payload === null)
       return {error: "Error: El payload no existe. Inténtelo nuevamente"}
-    }
     //Solo aplica con 3 parametros
-    if(typeof payload.id === "number" && typeof payload.username === "string" && typeof payload.name === "string"){
-      return {error: "no hay error"};
-    }else return {error: "Error: Los valores del payload necesitan tener un valor válido. Inténtelo nuevamente"}
+    if(typeof payload.id === "number" && typeof payload.username === "string" && typeof payload.name === "string")
+      return UserService.create(payload.id, payload.username, payload.name);
+    return {error: "Error: Los valores del payload necesitan tener un valor válido. Inténtelo nuevamente"}
     
     //Con n parametros
 
