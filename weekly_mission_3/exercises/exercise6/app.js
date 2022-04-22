f0ad1c4f70d936b721c1cf1dcde7a6fbd16ba149
felix <josevelazco134@gmail.com>
2022-04-21 22:29:59 -0600

    Agregar nueva ruta

--- weekly_mission_3/exercises/exercise6/app.js
+++ weekly_mission_3/exercises/exercise6/app.js
@@ -6,6 +6,10 @@ app.get('/', (req,res) => {
   res.send("Hello world");
 })
 
+app.get('/launchx', (req, res) => {
+  res.send('Bienvenidos a LaunchX');
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
   
