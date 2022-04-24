d53b64c294de78e18c8fee1575a71654dc2b3452
felix <josevelazco134@gmail.com>
2022-04-23 22:20:49 -0600

    Add post request

--- weekly_mission_3/exercises/exercise7/express_api/app.js
+++ weekly_mission_3/exercises/exercise7/express_api/app.js
@@ -19,6 +19,25 @@ app.get("/v1/explorers", (req,res) =>{
   res.status(200).json(explorers);
 })
 
+app.get('/v1/explorers/:id', (req, res) => {
+  console.log(`Api explorers GET Request ${new Date()}`);
+  console.log(`getting explorer with id ${req.params.id}`);
+  const explorer = {id: 1, name: "Felix"};
+  // const explorer1 = {id:1, name: "felix1"};
+  // const explorer2 = {id:2, name: "felix2"};
+  // const explorer3 = {id:3, name: "felix3"};
+  // const explorer4 = {id:4, name: "felix4"};
+  
+  // const explorers = [explorer1, explorer2, explorer3, explorer4];
+  res.status(200).json(explorer);
+})
+
+app.post('/v1/explorers', (req, res) => {
+  console.log(`Api explorers POST request ${new Date()}`);
+  const requestBody = req.body //Parametros de un cliente
+  res.status(201).json({message: "Created"});
+  
+})
 
 //Inicializar la app
 app.listen(port, ()=> {
