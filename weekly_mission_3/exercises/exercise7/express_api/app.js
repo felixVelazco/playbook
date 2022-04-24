cc7ed65622f45108b03b3752f7245490154a891e
felix <josevelazco134@gmail.com>
2022-04-23 20:52:13 -0600

    Create endpoint to get explorers list

--- weekly_mission_3/exercises/exercise7/express_api/app.js
+++ weekly_mission_3/exercises/exercise7/express_api/app.js
@@ -6,6 +6,20 @@ app.use(express.json()); //indicar que usaremos json
 
 const port = 3000;
 
+//HTTP methods
+app.get("/v1/explorers", (req,res) =>{
+  console.log(`Api explorers GET ALL requests ${new Date()}`);
+  const explorer1 = {id:1, name: "felix1"};
+  const explorer2 = {id:2, name: "felix2"};
+  const explorer3 = {id:3, name: "felix3"};
+  const explorer4 = {id:4, name: "felix4"};
+  
+  const explorers = [explorer1, explorer2, explorer3, explorer4];
+
+  res.status(200).json(explorers);
+})
+
+
 //Inicializar la app
 app.listen(port, ()=> {
   console.log(`Example app listening on port ${port}`); 
