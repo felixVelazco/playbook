d4c51f98012cd1fbdbff6346cbd1bda62eed1269
felix <josevelazco134@gmail.com>
2022-04-23 20:37:52 -0600

    funtion to send parameters

--- weekly_mission_3/exercises/exercise6/app.js
+++ weekly_mission_3/exercises/exercise6/app.js
@@ -15,6 +15,10 @@ app.get('/explorersInNode', (req, res) => {
   res.send(explorer);
 })
 
+app.get('/explorers/:name', (req, res) => {
+  res.send(req.params);
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
   
